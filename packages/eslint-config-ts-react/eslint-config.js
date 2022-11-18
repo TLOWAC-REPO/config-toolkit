78b315408fc485c11ddf9130fb0197ba39379fc5
Changhun Lee <ch.lee@danbicorp.com>
2022-11-18 17:43:53 +0900

    feat: eslint 초기 설정 추가

---
+++ packages/eslint-config-ts-react/eslint-config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "react", "react-hooks", "prettier"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "standard-with-typescript",
+    "prettier",
+  ],
+  rules: {
+    "prettier/prettier": ["error", { endOfLine: "auto" }],
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+};
